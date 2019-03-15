949b4f8373236831b5a72782dba84626fce91af3
mithlesh02 <46572883+mithlesh02@users.noreply.github.com>
2019-03-15 17:43:00 +0530

    Update lab3_4.s

--- lab3_4.s
+++ lab3_4.s
@@ -7,9 +7,11 @@ li $t1,34
 and  $t2,$t0,$t1
 or   $t3,$t0,$t1
 xor  $t4,$t0,$t1
-add  $t5,$t4,$zero
+
+nor  $t5,$t4,$zero
+
 xor  $t5,$t5,$t5
-nor  $t5,$t6,$zero
+nor  $t6,$t1,$zero
 andi $s0,$t2,15
 ori  $s1,$t2,3
 
